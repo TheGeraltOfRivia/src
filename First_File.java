d18d123a62e88850fdb581c08ad7534f5fb4f095
TheGeraltOfRivia <i201847@nu.edu.pk>
2021-12-04 21:45:06 +0500

    Initial commit

---
+++ First_File.java
@@ -0,0 +1,10 @@
+
+public class First_File {
+	public static void main(String[] args) {
+		String name = "Daniyal";
+		String rollno = "20i-1847";
+		System.out.println("Name: " + name);
+		System.out.println("RollNo : " + rollno);
+	}
+
+}
