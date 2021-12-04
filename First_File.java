fa8e95c8bfc6053be1980fa1c50522db0e91b3de
TheGeraltOfRivia <95530292+TheGeraltOfRivia@users.noreply.github.com>
2021-12-05 00:16:15 +0500

    Updated.java

--- First_File.java
+++ First_File.java
@@ -1,13 +1,13 @@
 
 public class First_File {
 	public static void main(String[] args) {
-		String name = "Daniyal";
-		String rollno = "20i-1847";
+		String name = "M Saad";
+		String rollno = "20i0914";
 		System.out.println("Name: " + name);
 		System.out.println("RollNo : " + rollno);
 
-		System.out.println("Hi my name is AAshir");
-		System.out.println(" Edited on eclipse");
+		System.out.println("Hi my name is Saad");
+		System.out.println(" Edited on IntelliJ Idea");
 	}
 
 }
